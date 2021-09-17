a91e18da8b44800bda26246048a2bc3bf5caa1e7
Fábio Macêdo Mendes <fabiomacedomendes@gmail.com>
2021-09-17 11:38:29 -0300

    Adicionou primeiro template

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # http://127.0.0.1/
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    return render(request, 'blog/post_list.html', {"nome": "World"})
 
 # http://127.0.0.1/read/<id>/
 def post_read(request, id):
