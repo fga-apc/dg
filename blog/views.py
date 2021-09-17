e4080234af15395417df982d16ec4d456fd2540d
Fábio Macêdo Mendes <fabiomacedomendes@gmail.com>
2021-09-17 12:12:24 -0300

    Atualizou templates

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-
+from blog.models import Post
+from django.utils import timezone
 
 # http://127.0.0.1/
 def post_list(request):
-    return render(request, 'blog/post_list.html', {"nome": "World"})
+    posts = (
+        Post.objects
+            .filter(published_date__lte=timezone.now())
+            .order_by('published_date')
+    )
+    return render(request, 'blog/post_list.html', {"posts": posts})
 
 # http://127.0.0.1/read/<id>/
 def post_read(request, id):
