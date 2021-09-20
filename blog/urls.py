931665d4d3249499a9558c3e6058bd54f6bf61ba
Fábio Macêdo Mendes <fabiomacedomendes@gmail.com>
2021-09-20 14:49:21 -0300

    Adicionou exemplo com views.add

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('read/<id>/', views.post_read, name='post_list'),
     path('edit/<id>/', views.post_edit, name='post_edit'),
     path('delete/<id>/', views.post_delete, name='post_delete'),
+    path('add/<x>/<y>/', views.add, name='add'),
     path('', views.post_list, name='post_list'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from blog.models import Post
 from django.utils import timezone
+from django.http import HttpResponse
 
 # http://127.0.0.1/
 def post_list(request):
@@ -13,7 +14,7 @@ def post_list(request):
 
 # http://127.0.0.1/read/<id>/
 def post_read(request, id):
-    ...
+    return HttpResponse(f'mostrando post {id}...')
 
 # http://127.0.0.1/edit/<id>/
 def post_edit(request, id):
@@ -22,3 +23,8 @@ def post_edit(request, id):
 # http://127.0.0.1/delete/<id>/
 def post_delete(request, id):
     ...
+
+
+def add(request, x, y):
+    variaveis = {"answer": int(x) + int(y)}
+    return render(request, 'blog/add.html', variaveis)
